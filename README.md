cc4e8aa222b382423686a6f81650fbba5bc9ee19
Wizard of z/OS <wizard@zdevops.com>
2022-04-04 22:44:53 +0200

    0.0.1 first stuffs

---
+++ README.md
@@ -0,0 +1,9 @@
+# pyracf
+
+## parsing IRRDBU00 unloads like a boss
+
+    from pyracf import RACF
+    RACF.parse('path/to/irrdbu00')
+    RACF.users.info()
+
+    
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyracf",
+    version="0.0.1",
+    author="Wizard of z/OS",
+    author_email="wizard@zdevops.com",
+    description="Parsing IRRDBU00 unloads in panda dataframes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wizardofzos/pyracf",
+    project_urls={
+        "Bug Tracker": "https://github.com/wizardofzos/pyracf/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
