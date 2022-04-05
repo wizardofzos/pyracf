5d556781ca1e5b07e1e3b21da560a823e5b85c79
Wizard of z/OS <wizard@zdevops.com>
2022-04-05 21:52:52 +0200

    More recordtypes, more query

--- src/pyracf/__init__.py
+++ src/pyracf/__init__.py
@@ -103,7 +103,7 @@ class RACF:
         # TODO: make this multiple threads (per record-type?)
         self._state = self.STATE_PARSING
         # TODO: Complete all record-types. Fix offsets.json !
-        thingswewant = ['0200']    
+        thingswewant = ['0100', '0102', '0200', '0400', '0404', '0500', '0505']    
         with open(self._irrdbu00, 'r', encoding="utf-8", errors="replace") as infile:
             for line in infile:
                 r = line[:4]
@@ -124,12 +124,29 @@ class RACF:
                             value = line[start-1:end].strip()
                             irrmodel[name] = str(value) 
                             
+                        if r == '0100':
+                            self.GPBD.append(irrmodel)
+                        if r == '0102':
+                            self.GPMEM.append(irrmodel)
                         if r == '0200':
-                            self.USBD.append(irrmodel)     
+                            self.USBD.append(irrmodel)   
+                        if r == '0400':
+                            self.DSBD.append(irrmodel)
+                        if r == '0404':
+                            self.DSACC.append(irrmodel)  
+                        if r == '0500': 
+                            self.GRBD.append(irrmodel)
+                        if r == '0505':
+                            self.GRACC.append(irrmodel)       
                     self._records[r]['parsed'] += 1
         # all models parsed :)
-        if '0200' in thingswewant:
-            self._users = pd.DataFrame.from_dict(self.USBD)  
+        self._users = pd.DataFrame.from_dict(self.USBD)  
+        self._groups = pd.DataFrame.from_dict(self.GPBD)
+        self._connects = pd.DataFrame.from_dict(self.GPMEM)
+        self._datasets = pd.DataFrame.from_dict(self.DSBD)
+        self._generics = pd.DataFrame.from_dict(self.GRBD)
+        self._datasetAccess = pd.DataFrame.from_dict(self.DSACC)
+        self._genericAccess = pd.DataFrame.from_dict(self.GRACC)
         
         self._state = self.STATE_READY         
         return True
@@ -146,3 +163,51 @@ class RACF:
         # TODO: Need more coolness here :)
 
         return self._users
+
+    def groups(self, query=None):
+        if self._state != self.STATE_READY:
+            raise StoopidException('Not done parsing yet! (PEBKAM/ID-10T error)')
+        
+        return self._groups
+
+    def datasets(self, query=None):
+        if self._state != self.STATE_READY:
+            raise StoopidException('Not done parsing yet! (PEBKAM/ID-10T error)')
+        
+        return self._datasets
+
+    def connects(self, query=None):
+        if self._state != self.STATE_READY:
+            raise StoopidException('Not done parsing yet! (PEBKAM/ID-10T error)')
+        
+        return self._connects
+
+    def datasetAccess(self, query=None):
+        if self._state != self.STATE_READY:
+            raise StoopidException('Not done parsing yet! (PEBKAM/ID-10T error)')
+        
+        if query == "orphans":
+            self._datasetAccess = self._datasetAccess.assign(inGroups=self._datasetAccess.DSACC_AUTH_ID.isin(self._groups.GPBD_NAME))
+            self._datasetAccess = self._datasetAccess.assign(inUsers=self._datasetAccess.DSACC_AUTH_ID.isin(self._users.USBD_NAME))
+            # So we can select all dataset profiles that have an orphan profile on the accesslist
+            return self._datasetAccess.loc[(self._datasetAccess['inGroups'] == False) & (self._datasetAccess['inUsers'] == False) & (self._datasetAccess['DSACC_AUTH_ID'] != "*") & (self._datasetAccess['DSACC_AUTH_ID'] != "&RACUID")]
+            
+        return self._datasetAccess
+
+    def generics(self, query=None):
+        if self._state != self.STATE_READY:
+            raise StoopidException('Not done parsing yet! (PEBKAM/ID-10T error)')
+        
+        return self._generics
+
+    def genericAccess(self, query=None):
+        if self._state != self.STATE_READY:
+            raise StoopidException('Not done parsing yet! (PEBKAM/ID-10T error)')
+        
+        if query == "orphans":
+            self._genericAccess = self._genericAccess.assign(inGroups=self._genericAccess.GRACC_AUTH_ID.isin(self._groups.GPBD_NAME))
+            self._genericAccess = self._genericAccess.assign(inUsers=self._genericAccess.GRACC_AUTH_ID.isin(self._users.USBD_NAME))
+            return self._genericAccess.loc[(self._genericAccess['inGroups'] == False) & (self._genericAccess['inUsers'] == False) & (self._genericAccess['GRACC_AUTH_ID'] != "*") & (self._genericAccess['GRACC_AUTH_ID'] != "&RACUID")]
+
+        return self._genericAccess
+    
