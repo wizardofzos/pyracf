0e0015fc39bbab4e202840b53ad09f22c6147baf
Rob van Hoboken <rob.on.lists@gmail.com>
2024-02-14 00:01:06 +0100

    more intuitive wrapper name in deprecated function

--- src/pyracf/__init__.py
+++ src/pyracf/__init__.py
@@ -25,17 +25,17 @@ class StoopidException(Exception):
         super().__init__(self.message)
 
 def deprecated(func,oldname):
-    ''' Wrapper routine to add (old) alias name to new routine (func), supports methods and properties. 
+    ''' Wrapper routine to add (deprecated) alias name to new routine (func), supports methods and properties. 
         Inspired by functools.partial() '''
-    def newfunc(*arg,**keywords):
+    def deprecated_func(*arg,**keywords):
         if hasattr(func,"__name__"):  # normal function object
             newroutine = func
         else:  # property object
             newroutine = func.fget
         warnings.warn(f"{oldname} is deprecated and will be removed, use {newroutine.__name__} instead.")
         return newroutine(*arg,**keywords)
-    newfunc.func = func
-    return newfunc
+    deprecated_func.func = func
+    return deprecated_func
 
 class RACF:
     
